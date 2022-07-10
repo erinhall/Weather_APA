2aced586eae37a26024148dc73d821c1014a5a09
erinhall <erin.rae.hall@gmail.com>
2022-07-10 13:58:53 -0500

    Initial commit

---
+++ Weather/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weather.Models;
+
+namespace Weather.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new WeatherModel());
+        }
+
+        [HttpPost]
+        public ActionResult GetWeather(string city)
+        {
+            //initialize model and call getweatherlist to make api call using user inputted city from ui
+            WeatherModel weather = new WeatherModel();
+            weather.City = city;
+            return Json(weather.GetWeatherList(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Weather/Models/Weather.cs
@@ -0,0 +1,136 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text;
+
+
+namespace Weather.Models
+{
+    //user defined model for api data
+    public class WeatherModel
+    {
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(100)]
+        [Display(Name = "Enter city")]
+        public string City { get; set; }
+
+        private static string appId = "6959648cee0480737e40bf1aab6c1936";
+
+        /// <returns>Object</returns>
+        public Object GetWeatherList()
+        {
+            Object jsonContent;
+            
+            //build url string
+            StringBuilder builder = new StringBuilder();
+          
+            builder.Append("https://api.openweathermap.org/data/2.5/forecast?q=");
+            builder.Append(City);
+            builder.Append("&APPID=");
+            builder.Append(appId);
+            builder.Append("&units=imperial");
+            string url = builder.ToString();
+
+            //make api call with webclient and deserialize json response into object
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    var content = client.DownloadString(url);
+                    jsonContent = JsonConvert.DeserializeObject<RootObject>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                return "not found";
+            }
+            return jsonContent;
+        }
+    }
+
+    //all needed classes/properties
+    public class Main
+    {
+        public double temp { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public int pressure { get; set; }
+        public int sea_level { get; set; }
+        public int grnd_level { get; set; }
+        public int humidity { get; set; }
+        public double temp_kf { get; set; }
+     
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public int deg { get; set; }
+    }
+
+    public class Sys
+    {
+        public string pod { get; set; }
+    }
+
+    public class Rain
+    {
+        public double __invalid_name__3h { get; set; }
+    }
+
+    public class List
+    {
+        public int dt { get; set; }
+        public Main main { get; set; }
+        public List<Weather> weather { get; set; }
+        public Clouds clouds { get; set; }
+        public Wind wind { get; set; }
+        public Sys sys { get; set; }
+        public string dt_txt { get; set; }
+        public Rain rain { get; set; }
+    }
+
+    public class Coord
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+
+    public class City
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Coord coord { get; set; }
+        public string country { get; set; }
+        public int population { get; set; }
+        public int timezone { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string cod { get; set; }
+        public int message { get; set; }
+        public int cnt { get; set; }
+        public List<List> list { get; set; }
+        public City city { get; set; }
+    }
+}
